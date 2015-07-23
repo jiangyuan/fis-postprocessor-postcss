30611e565892baf4105b4f6d02c50c91d543c45b
jiangyuan <jiangyuanlucky@gmail.com>
2015-07-23 16:03:40 +0800

    0.1.0

--- README.md
+++ README.md
@@ -1,19 +1,48 @@
-# fis-postprocessor-postcss
-fis 的 [postcss](https://github.com/postcss/postcss) 插件。
+fis-postprocessor-postcss
+---------------
 
-**完善中...**
+fis 的 [postcss](https://github.com/postcss/postcss) 插件。
+内置 [cssnext](http://cssnext.io/) 和 [autoprefixer](https://github.com/postcss/autoprefixer) 。
 
 ## 使用
-fis-config.js
+### 安装
+`npm i fis-postprocessor-postcss -g`
 
+### 配置
 ```js
 fis.config.merge({
     modules: {
         postprocessor : {
-            sass: 'postcss',
-            scss: 'postcss',
             css: 'postcss'
         }
     }
 });
 ```
+
+### 参数
+```js
+settings: {
+    postprocessor: {
+        postcss: {
+            autoprefixer: false, // 默认 true
+            cssnext: false, // 默认 false
+            customPlugin: function(plugins) {   // 参数 plugins 是内置插件
+                plugins = plugins.push(require('postcss-border'));
+                return plugins; // 返回新的插件列表
+            }
+        }
+    }
+}
+```
+
+### demo 说明
+安装完插件后，切换至 demo/src 目录下执行：
+```
+fis release -d dev [-mD]
+```
+
+## 关于 cssnext
+目前还不能和 `sass` 混用，即 '.scss' 文件中不要掺杂 `cssnext` 语法。
+
+## 关于 cssgrace
+在 fis `postprocessor` 中， [cssgrace](https://github.com/cssdream/cssgrace) 处理图片路径有问题，不建议使用。
--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 var postcss = require('postcss');
-var autoprefixer = require('autoprefixer');
-// var cssnext = require('cssnext');
-var cssgrace = require('cssgrace');
+var P = {
+    autoprefixer: require('autoprefixer'),
+    cssnext: require('cssnext')()
+};
+
+var def = {
+    cssnext: false,
+    autoprefixer: false,
+    customPlugin: function () {}
+};
 
 module.exports = function (content, file, conf) {
-
     if (file.isCssLike) {
-        content = postcss([autoprefixer, /*cssnext, */cssgrace]).process(content).css;
-        console.log('-------------');
-        console.log(content);
+        var opts = fis.util.merge(def, conf);
+        var plugins = [];
+
+        if (opts.cssnext) {
+            plugins.push(P.cssnext);
+            opts.autoprefixer = false;  // cssnext 自带 autoprefixer
+        }
+
+        if (opts.autoprefixer) {
+            plugins.push(P.autoprefixer);
+        }
+
+        var tmp = opts.customPlugin.call(this, plugins, content, file, conf);
+        if (Array.isArray(tmp) && tmp.length) {
+            plugins = tmp;
+        }
+
+        if (!plugins.length) {
+            return content;
+        }
+
+        content = postcss(plugins).process(content).css;
     }
 
     return content;
